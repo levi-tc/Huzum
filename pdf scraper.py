2664a283f327c23214d3249a6ec1cc9e6e08f37b
dqnyb <dqny2018@gmail.com>
2023-10-21 15:03:28 +0300

    1

--- pdf scraper.py
+++ pdf scraper.py
@@ -1,16 +1,20 @@
 import fitz  # PyMuPDF
 
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with fitz.open(pdf_path) as pdf_document:
+        num_pages = pdf_document.page_count
+        for page_num in range(num_pages):
+            page = pdf_document.load_page(page_num)
+            text += page.get_text()
+    return text
 
-def read_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-
-    for page_num in range(doc.page_count):
-        page = doc[page_num]
-        text = page.get_text()
-        print(f"Page {page_num + 1}:\n{text}\n")
-
-    doc.close()
+def write_text_to_file(text, output_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(text)
 
 pdf_path = "data_unstr/1.pdf"  # Replace with your PDF file path
-read_pdf(pdf_path)
+output_file = "data_unstr/1.txt"  # Replace with the desired output file name
 
+extracted_text = extract_text_from_pdf(pdf_path)
+write_text_to_file(extracted_text, output_file)
